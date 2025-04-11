5b27614574798d5421b96ffd3871145d2a71d380
dasaraul <tamskun29@gmail.com>
2025-04-11 17:28:04 +0700

    change

--- plugins/owner/setwm.js
+++ plugins/owner/setwm.js
@@ -11,7 +11,7 @@ exports.run = {
    }) => {
       try {
          let setting = global.db.setting
-         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'Sticker by | @neoxrs'), m)
+         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'Sticker by | Tamaengs'), m)
          let [packname, ...author] = text.split`|`
          author = (author || []).join`|`
          setting.sk_pack = packname || ''
